8af24f4aae1581712eb33ac30b8adaaf569f03e5
Daniele Bisello <daniele.bisello@chilicode.it>
2024-10-08 11:17:35 +0200

    gitignore

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['message' => 'Hello from slave laravel!', 'timestamp' => now()];
 });
